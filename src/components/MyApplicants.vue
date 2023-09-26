be9e6f820d1ff30524466dfe5ca37c15a5110446
alxsjx <116423780+alxsjx@users.noreply.github.com>
2023-09-26 17:49:49 +0800

    Update my applicants (ver 1)
    updated boldness of fonts and made progress bar adjusted based on amount

--- src/components/MyApplicants.vue
+++ src/components/MyApplicants.vue
@@ -1,100 +1,111 @@
 <template>
-    <div>
-      <!-- Header -->
-      <div class="header">
-        My Applicants
+  <div>
+    <!-- Header -->
+    <div class="header">
+      My Applicants
+    </div>
+
+    <!-- Container for Cards -->
+    <div class="container">
+      <!-- Card 1 -->
+      <div class="card" @click="handleCardClick(0)">
+        <div class="profile-picture">
+          <img :src="applicants[0].profilePicture" alt="Profile Picture">
+        </div>
+        <div class="details">
+          <div class="name">{{ applicants[0].name }}</div>
+          <div class="position">{{ applicants[0].position }}</div>
+        </div>
+        <div class="progress-bar" :style="getProgressBarStyle(applicants[0].matchPercentage)">
+          <div class="progress-text">
+            {{ applicants[0].matchPercentage }}%
+            <span class="match-text">Match</span>
+          </div>
+        </div>
       </div>
-  
-      <!-- Container for Cards -->
-      <div class="container">
-        <!-- Card 1 -->
-        <div class="card" @click="handleCardClick(0)">
-          <div class="profile-picture">
-            <img :src="applicants[0].profilePicture" alt="Profile Picture">
-          </div>
-          <div class="details">
-            <div class="name">{{ applicants[0].name }}</div>
-            <div class="position">{{ applicants[0].position }}</div>
-          </div>
-          <div class="progress-bar">
-            <div class="progress-text">
-              {{ applicants[0].matchPercentage }}%
-              <span class="match-text">Match</span>
-            </div>
+
+      <!-- Card 2 -->
+      <div class="card" @click="handleCardClick(1)">
+        <div class="profile-picture">
+          <img :src="applicants[1].profilePicture" alt="Profile Picture">
+        </div>
+        <div class="details">
+          <div class="name">{{ applicants[1].name }}</div>
+          <div class="position">{{ applicants[1].position }}</div>
+        </div>
+        <div class="progress-bar" :style="getProgressBarStyle(applicants[1].matchPercentage)">
+          <div class="progress-text">
+            {{ applicants[1].matchPercentage }}%
+            <span class="match-text">Match</span>
           </div>
         </div>
-  
-        <!-- Card 2 -->
-        <div class="card" @click="handleCardClick(1)">
-          <div class="profile-picture">
-            <img :src="applicants[1].profilePicture" alt="Profile Picture">
-          </div>
-          <div class="details">
-            <div class="name">{{ applicants[1].name }}</div>
-            <div class="position">{{ applicants[1].position }}</div>
-          </div>
-          <div class="progress-bar">
-            <div class="progress-text">
-              {{ applicants[1].matchPercentage }}%
-              <span class="match-text">Match</span>
-            </div>
-          </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="card" @click="handleCardClick(2)">
+        <div class="profile-picture">
+          <img :src="applicants[2].profilePicture" alt="Profile Picture">
         </div>
-  
-        <!-- Card 3 -->
-        <div class="card" @click="handleCardClick(2)">
-          <div class="profile-picture">
-            <img :src="applicants[2].profilePicture" alt="Profile Picture">
-          </div>
-          <div class="details">
-            <div class="name">{{ applicants[2].name }}</div>
-            <div class="position">{{ applicants[2].position }}</div>
-          </div>
-          <div class="progress-bar">
-            <div class="progress-text">
-              {{ applicants[2].matchPercentage }}%
-              <span class="match-text">Match</span>
-            </div>
+        <div class="details">
+          <div class="name">{{ applicants[2].name }}</div>
+          <div class="position">{{ applicants[2].position }}</div>
+        </div>
+        <div class="progress-bar" :style="getProgressBarStyle(applicants[2].matchPercentage)">
+          <div class="progress-text">
+            {{ applicants[2].matchPercentage }}%
+            <span class="match-text">Match</span>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        applicants: [
-          {
-            name: 'Seth Yap',
-            position: 'Lead Product Designer',
-            profilePicture: 'profile.jpg',
-            matchPercentage: 68,
-          },
-          {
-            name: 'John Doe',
-            position: 'Web Developer',
-            profilePicture: 'profile2.jpg',
-            matchPercentage: 75,
-          },
-          {
-            name: 'Alice Smith',
-            position: 'Graphic Designer',
-            profilePicture: 'profile3.jpg',
-            matchPercentage: 80,
-          },
-        ],
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      applicants: [
+        {
+          name: 'Seth Yap',
+          position: 'Lead Product Designer',
+          profilePicture: 'profile.jpg',
+          matchPercentage: 68,
+        },
+        {
+          name: 'John Doe',
+          position: 'Web Developer',
+          profilePicture: 'profile2.jpg',
+          matchPercentage: 75,
+        },
+        {
+          name: 'Alice Smith',
+          position: 'Graphic Designer',
+          profilePicture: 'profile3.jpg',
+          matchPercentage: 20,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleCardClick(index) {
+      // Handle card click event, e.g., navigate to the applicant's details page
+      console.log(`Clicked on card ${index}`);
     },
-    methods: {
-      handleCardClick(index) {
-        // Handle card click event, e.g., navigate to applicant's details page
-        console.log(`Clicked on card ${index}`);
-      },
+    getProgressBarStyle(matchPercentage) {
+      // Determine the color of the progress bar based on matchPercentage
+        return {
+          
+          background: `
+          radial-gradient(closest-side, white 79%, transparent 80% 100%),
+          conic-gradient(#6A44D4 ${matchPercentage}% , #b3b3b3 0)`,
+        };
+      }
     },
   };
-  </script>
+</script>
+
+
  <style scoped>
  .header {
    font-family: 'montserrat-bold';
@@ -125,7 +136,6 @@
    transition: background-color 0.3s, color 0.3s;
    cursor: pointer;
    color: black;
-   font-family: 'montserrat-bold';
  }
  
  .card:hover {
@@ -137,13 +147,6 @@
    border: 2px solid #FFFFFF; /* Progress bar border color on hover */
  }
  
- .card:hover .progress-text {
-   color: #FFFFFF; /* Text color inside progress bar on hover */
- }
-
- .card:hover .position {
-   color: #ffffffdc; /* Text color inside progress bar on hover */
- }
  
  .profile-picture {
    width: 80px;
@@ -167,19 +170,18 @@
  
  .name {
    font-size: 18px;
-   font-weight: bold;
+   font-family: 'montserrat-bold';
  }
  
  .position {
    font-size: 14px;
-   color: #857F95;
+   color: #b3b3b3;
  }
  
  .progress-bar {
    width: 80px;
    height: 80px;
    border-radius: 50%;
-   border: 2px solid #6A44D4;
    display: flex;
    align-items: center;
    justify-content: center;
@@ -194,6 +196,7 @@
    flex-direction: column;
    align-items: center;
    text-align: center;
+   font-family: 'montserrat-bold';
  }
  </style>
  
