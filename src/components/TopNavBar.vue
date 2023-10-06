b3fbf35bb3742d17704810a1f5b5046e8fcdfa2f
tan-sd <shengdatan@gmail.com>
2023-10-07 00:15:06 +0800

    Initialise TopNavBar component

---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+</script>
+
+<template>
+  <div class="navBarContainer mb-3">
+    <div class="backButtonContainer">
+      <font-awesome-icon
+        :icon="['fas', 'chevron-left']"
+        size="2xl"
+        @click="navigateBack"
+        style="color: #ffffff"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateBack() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navBarContainer {
+  width: 100%;
+  height: 75px;
+  background-color: #160b32;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  color: #ffffff;
+  padding-left: 2em;
+}
+
+.backButtonContainer {
+  position: relative;
+  cursor: pointer;
+}
+</style>
