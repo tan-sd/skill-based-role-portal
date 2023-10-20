3d86dca2a962fc49f48427d1ce5234f23d7dba89
tan-sd <shengdatan@gmail.com>
2023-10-20 22:11:40 +0800

    Formatting. Add listing details and edit router params variable names

--- src/components/MyApplicants.vue
+++ src/components/MyApplicants.vue
@@ -1,20 +1,59 @@
 <script setup>
-import { getStaffObj } from '../firebase/staff_class';
-import Listing from '../firebase/listing_class';
+import { getStaffObj } from '../firebase/staff_class'
+import Listing from '../firebase/listing_class'
 import TopNavBar from './TopNavBar.vue'
 </script>
 <template>
   <div>
     <TopNavBar />
-    <div class="d-flex flex-column h-100">
+    <div class="d-flex flex-column w-100">
       <div class="header-container px-4 py-4">
         <!-- Header -->
-        <h3 class="fw-bold">My Applicants</h3>
+        <h3 class="fw-bold">Listing Details</h3>
         <button class="btn btn-secondary text-white px-5" type="button" @click="edit_clicked()">
           Edit Listing
         </button>
       </div>
+      <div class="bg-white rounded m-3 p-4">
+        <div class="row">
+          <div class="col-7">
+            <!-- Job Description -->
+            <h5 class="fw-bold">Job Description</h5>
+            <p>{{ listingDetails.description }}</p>
 
+            <!-- Job Requirements -->
+            <h5 class="fw-bold mt-4">Job Requirements</h5>
+            <ul>
+              <li :key="index" v-for="(req, index) in listingDetails.responsibilities">
+                {{ req }}
+              </li>
+            </ul>
+
+            <!-- Required Skills -->
+            <h5 class="fw-bold">Required Skills</h5>
+            <div
+              v-for="(e_skill, index) in listingDetails.skills"
+              :key="index"
+              class="mb-1 me-2 p-1 px-2 text-white rounded d-inline-block"
+              :class="userSkills.includes(e_skill) ? 'bg-primary' : 'bg-light2'"
+            >
+              {{ e_skill }}
+            </div>
+          </div>
+          <div class="col-5 position-relative">
+            <!-- Application Period -->
+            <p>
+              <span class="fw-bold add-border-right">Application Period</span>
+              {{ toHumanReadbleDate(listingDetails.createdate) }} to
+              {{ toHumanReadbleDate(listingDetails.deadline) }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="header-container px-4 py-4">
+        <!-- Header -->
+        <h3 class="fw-bold">My Applicants</h3>
+      </div>
       <!-- Container for Cards -->
       <div class="body-container container-fluid px-4">
         <!-- Conditionally render loop if newAppList has data -->
@@ -22,15 +61,14 @@ import TopNavBar from './TopNavBar.vue'
           <router-link
             v-for="applicant in newAppList"
             :key="applicant.name"
-            :to="`/individualApplicant/${applicant.id}`"
+            :to="`/${applicant.listingId}/individualApplicant/${applicant.staffId}`"
             class="card border-0 my-3 p-3 bg-white flex-col flex-row listing-card justify-content-start"
           >
-
             <div class="profile-picture">
               <img :src="applicant.profilePic" alt="Profile Picture" />
             </div>
             <div class="details d-inline">
-              <div class="name">{{ applicant.name}}</div>
+              <div class="name">{{ applicant.name }}</div>
               <div class="position">{{ applicant.position }}</div>
             </div>
             <!-- Only display the progress-bar when matchPercentage is defined -->
@@ -47,10 +85,7 @@ import TopNavBar from './TopNavBar.vue'
           </router-link>
         </div>
         <!-- Display a loading message if newAppList is empty -->
-        <div
-          v-else
-          class="d-flex flex-column align-items-center justify-content-center h-100"
-        >
+        <div v-else class="d-flex flex-column align-items-center justify-content-center h-100">
           <p><font-awesome-icon icon="fa-solid fa-ghost" class="text-light2" size="5x" /></p>
           <h4 class="fw-bold">OOPS! Looks like you have no applicants yet...</h4>
         </div>
@@ -63,52 +98,47 @@ import TopNavBar from './TopNavBar.vue'
 export default {
   data() {
     return {
-      job: {
-        listingDetails: {
-
-        },
-        jobSkills: [],
-        applicantsList: []
+      listingDetails: {
       },
+      userSkills: [],
       newAppList: [] // Initialize newAppList as an empty array
     }
   },
   async created() {
-    
     const listing = await this.fetchIndividualListingData()
     await this.fetch_read_staff_data(listing)
-    console.log(this.newAppList)
-    
-
 
     // Populate newAppList
-    
   },
   methods: {
     async fetchIndividualListingData() {
       try {
-        const listing = await new Listing()
-        await listing.loadListing(this.$route.params.id)
+        const listing = new Listing()
+        await listing.loadListing(this.$route.params.listingid)
+        this.listingDetails = listing.getAllAtrr()
         return listing
-
       } catch (error) {
         console.log('Error fetching data from Firebase:', error)
       }
     },
     async fetch_read_staff_data(listing) {
       for (let i = 0; i < listing.getApplicants().length; i++) {
-      const response = await getStaffObj(listing.getApplicants()[i])
-      const tempObj = {
-        id: response.getID(),
-        name: response.getFullName(),
-        position: response.getPosition(),
-        applicantSkills: response.getSkillset(),  
-        profilePic: response.getProfilePic(),
-        matchPercentage: this.calculateMatchPercentage(listing.getSkills(), response.getSkillset())
-      }
+        const response = await getStaffObj(listing.getApplicants()[i])
+        const tempObj = {
+          listingId: this.listingDetails.listingId,
+          staffId: response.getID(),
+          name: response.getFullName(),
+          position: response.getPosition(),
+          applicantSkills: response.getSkillset(),
+          profilePic: response.getProfilePic(),
+          matchPercentage: this.calculateMatchPercentage(
+            listing.getSkills(),
+            response.getSkillset()
+          )
+        }
 
-      this.newAppList.push(tempObj) // Push data to newAppList
-    }
+        this.newAppList.push(tempObj) // Push data to newAppList
+      }
     },
     calculateMatchPercentage(reqs, skills) {
       // Ensure both input arrays are non-empty
@@ -142,25 +172,30 @@ export default {
           conic-gradient(#6A44D4 ${matchPercentage}% , #b3b3b3 0)`
       }
     },
-
     edit_clicked() {
-      var listing_id = this.$router.currentRoute.value.params.id
-      this.$router.push({ name: 'editListing', params: { id: listing_id } })
+      var listing_id = this.$router.currentRoute.value.params.listingid
+      this.$router.push({ name: 'editListing', params: { listingid: listing_id } })
+    },
+    async getUserSkills() {
+      const user_id = localStorage.getItem('id')
+
+      try {
+        const staff = await getStaffObj(user_id)
+        const user_skills = staff.getSkillset()
+
+        this.userSkills = user_skills
+      } catch (error) {
+        console.log('Error fetching data from Firebase:', error)
+      }
+    },
+    toHumanReadbleDate(date) {
+      const dateObj = new Date(date)
+      const options = { day: '2-digit', month: 'short', year: 'numeric' }
+      return dateObj.toLocaleDateString('en-US', options)
     }
   },
-  computed: {
-    getMatchPercentage() {
-      let skillsApplicantMatchcount = 0
-      for (let i = 0; i < this.job.jobSkills.length; i++) {
-        if (this.job.applicantSkills.includes(this.job.jobSkills[i])) {
-          skillsApplicantMatchcount++
-        }
-      }
-      const matchPercentage = Math.round(
-        (skillsApplicantMatchcount / this.job.jobSkills.length) * 100
-      )
-      return matchPercentage
-    }
+  mounted() {
+    this.getUserSkills()
   }
 }
 </script>
